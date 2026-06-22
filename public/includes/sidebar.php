47d1e79ab0cdfb50b803246de835fa313da669c3
unknown <karija-dev@wysiwygdev.ad.finsecur.lan>
2026-06-22 13:36:28 +0300

    remove menu on prod

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -34,12 +34,12 @@ require_once __DIR__ . '/functions.php';
           <div data-i18n="Page 1">Tableau de bord</div>
         </a>
       </li>
-      <li class="menu-item <?php echo isActivePage('stats'); ?>">
+      <!-- <li class="menu-item <?php echo isActivePage('stats'); ?>">
         <a href="<?php echo BASE_URL; ?>stats" class=" menu-link">
           <i class="menu-icon bi bi-bar-chart-line"></i>
           <div data-i18n="Page 1">Statistique</div>
         </a>
-      </li>
+      </li> -->
       <?php
       // Fonction pour détecter si on est sur une page d'intervention
       $currentUrl = $_SERVER['REQUEST_URI'] ?? '';
@@ -139,12 +139,12 @@ require_once __DIR__ . '/functions.php';
       </li>
 
       <!-- Matériel & Documentation - Nouvelle entrée pour Staff -->
-      <li class="menu-item <?php echo isActivePage('materiel_documentation'); ?>">
+      <!-- <li class="menu-item <?php echo isActivePage('materiel_documentation'); ?>">
         <a href="<?php echo BASE_URL; ?>materiel_documentation" class="menu-link">
           <i class="menu-icon bi bi-hdd-stack"></i>
           <div data-i18n="materiel_documentation">Matériel & Docs</div>
         </a>
-      </li>
+      </li> -->
 
       <li class="menu-item <?php echo isActivePage('materiel'); ?>">
         <a href="<?php echo BASE_URL; ?>materiel" class="menu-link">
