1c433468c5468d51ad582207390c7d9a8f11af8c
dev-ioc <dev_mdg@caspeo.fr>
2026-07-09 12:43:54 +0300

    modif logo Avance menu

--- public/includes/sidebar.php
+++ public/includes/sidebar.php
@@ -153,16 +153,9 @@ require_once __DIR__ . '/functions.php';
         </a>
       </li>
 
-      <li class="menu-item <?php echo isActivePage('documentation'); ?>">
-        <a href="<?php echo BASE_URL; ?>documentation" class="menu-link">
-          <i class="menu-icon bi bi-book"></i>
-          <div data-i18n="documentation">Documentation</div>
-        </a>
-      </li>
-      <!-- Lien externe vers Avance -->
-      <li class="menu-item">
+      <li class="menu-item  <?php echo isActivePage('avance'); ?></li>">
         <a href="https://avance-vdsc.vercel.app/" class="menu-link" target="_blank" rel="noopener noreferrer">
-          <i class="menu-icon bi bi-box-arrow-up-right"></i>
+          <img src="<?php echo BASE_URL; ?>assets/img/Avance.png" class="menu-icon avance-logo" alt="Avance">
           <div data-i18n="avance">Avance</div>
         </a>
       </li>
